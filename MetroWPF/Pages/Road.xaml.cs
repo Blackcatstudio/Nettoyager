93b904d829d0387d0e6e277a2c024688396e570d
Desgging <1397755924@qq.com>
2016-08-21 12:06:31 +0800

    Revert "Revert "添加了部分选择文件夹的代码""
    This reverts commit b5bb834f521695b9c090f3133d94f54fde022e28.

--- MetroWPF/Pages/Road.xaml.cs
+++ MetroWPF/Pages/Road.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +21,27 @@ namespace MetroWPF.Pages
     /// </summary>
     public partial class Road : Page
     {
+        string mcPath="";
         public Road()
         {
             InitializeComponent();
+        }
+
+        private void RoadChoose_Click(object sender, RoutedEventArgs e)
+        {
+            string PathTemp;
+            FolderBrowserDialog browseMCFolder = new FolderBrowserDialog();
+            browseMCFolder.Description = "选择\".minecraft\"文件夹";
+            DialogResult browseResult = browseMCFolder.ShowDialog();
+            if (browseResult != DialogResult.Cancel)
+            {
+                PathTemp = browseMCFolder.SelectedPath;
+                if(PathTemp.Substring(PathTemp.Length - 10,10)==".minecraft")
+                {
+                    txtPath.Text = PathTemp;
+                    mcPath = PathTemp;
+                }
+            }
         }
     }
 }
